4e59aa9f2ae9a1555dcb3d31e83b60dd0ab73063
s.ichinose <sae.ichinose@gmail.com>
2018-08-22 08:57:52 +0900

    テストプロジェクトでのライブラリ動作確認 動作済

--- Osoba/Osoba/Osoba.Term/Program.cs
+++ Osoba/Osoba/Osoba.Term/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Osoba.Builder.Idols;
 namespace Osoba.Term
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IdolsDirector d1 = new IdolsDirector(new CoolIdolBuilder());
+            IdolsDirector d2 = new IdolsDirector(new CuteIdolBuilder());
+            Idols idol1 = d1.Construct();
+            Idols idol2 = d2.Construct();
+
+
+            Console.WriteLine(idol1.Name);
+            Console.WriteLine(idol2.Name);
+
+            Console.Read();
         }
     }
 }
