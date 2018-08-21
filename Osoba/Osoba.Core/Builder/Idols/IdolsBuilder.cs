a7aadfc8f01c718348d5a4962f57998ebfc1f32a
s.nakamura <nakamura.shoto.59@gmail.com>
2018-08-21 19:07:06 +0900

    Builder test

---
+++ Osoba/Osoba.Core/Builder/Idols/IdolsBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osoba.Core.Builder.Idols
+{
+    //public abstract class IdolsBuilder
+    //{
+    //    public abstract void SetName();
+    //    public abstract void SetHead();
+    //    public abstract void SetTops();
+    //    public abstract void SetBottoms();
+    //    public abstract void SetShoes();
+    //    public abstract void SetAccessory();
+    //    public abstract void SetHp();
+    //    public abstract void SetMp();
+    //    public abstract void SetSp();
+    //    public abstract Idols GetIdol();
+    //}
+    public interface IdolsBuilder
+    {
+        void SetName();
+        void SetIdolCategoly();
+        void SetHead();
+        void SetTops();
+        void SetBottoms();
+        void SetShoes();
+        void SetAccessory();
+        void SetHp();
+        void SetMp();
+        void SetSp();
+        Idols GetIdol();
+    }
+}
