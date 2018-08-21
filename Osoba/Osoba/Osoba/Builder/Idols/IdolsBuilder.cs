adb0c1ce9bcf5aa0c32493ff88588a53673c5363
s.ichinose <sae.ichinose@gmail.com>
2018-08-22 01:37:09 +0900

    project convert to vs2017

---
+++ Osoba/Osoba/Osoba/Builder/Idols/IdolsBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osoba.Core.Builder.Idols
+{
+    public interface IdolsBuilder
+    {
+        void SetName();
+        void SetIdolCategoly();
+        void SetHead();
+        void SetTops();
+        void SetBottoms();
+        void SetShoes();
+        void SetAccessory();
+        void SetHp();
+        void SetMp();
+        void SetSp();
+        Idols GetIdol();
+    }
+}
---
+++ Osoba/Osoba/Osoba/Builder/Idols/IdolsDirector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osoba.Builder.Idols
+{
+    public class IdolsDirector
+    {
+        private IdolsBuilder builder;
+        public IdolsDirector(IdolsBuilder builder)
+        {
+            this.builder = builder;
+        }
+        public Idols Construct()
+        {
+            this.builder.SetName();
+            this.builder.SetIdolCategoly();
+            this.builder.SetHead();
+            this.builder.SetTops();
+            this.builder.SetBottoms();
+            this.builder.SetShoes();
+            this.builder.SetAccessory();
+            this.builder.SetHp();
+            this.builder.SetMp();
+            this.builder.SetSp();
+            return this.builder.GetIdol();
+        }
+    }
+}
